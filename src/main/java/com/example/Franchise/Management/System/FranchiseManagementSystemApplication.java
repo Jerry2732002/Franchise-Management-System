9979bb1c603e269dd08703387dc5dbc628cbbce2
Jerry2732002 <jerrysebastian2732002@gmail.com>
2025-02-06 10:04:07 +0530

    inital push

---
+++ src/main/java/com/example/Franchise/Management/System/FranchiseManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Franchise.Management.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranchiseManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FranchiseManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Franchise/Management/System/FranchiseManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Franchise.Management.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranchiseManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
